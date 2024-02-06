425bf49404d05c2f2e7f1d58e0c2f6b5edb4a621
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-02-06 21:17:05 +0100

    oke

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -17,6 +17,11 @@ function MyMain() {
       <Button className=" btn-red-600"> BOTTONE KEBAB</Button>
       <Button className=" btn-red-700"> BOTTONE KEBAB</Button>
       <Button className=" btn-red-800"> BOTTONE KEBAB</Button>
+      <Button className=" btn-brown-900"> BOTTONE KEBAB</Button>
+      <Button className=" btn-brown-800"> BOTTONE KEBAB</Button>
+      <Button className=" btn-brown-700"> BOTTONE KEBAB</Button>
+      <Button className=" btn-brown-600"> BOTTONE KEBAB</Button>
+      <Button className=" btn-brown-500"> BOTTONE KEBAB</Button>
     </Container>
   );
 }
